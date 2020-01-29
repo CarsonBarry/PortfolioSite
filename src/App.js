560a8d1acd0d0512285205aeb55fa19afc43ad55
Carson Barry <CarsonBarry374@gmail.com>
2020-01-29 12:42:34 -0600

    rebuild page structure pass 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import React from "react";
-import "./styles.css";
 
-{/* Note */}
+{/* NoteTemplate */}
+{/* ========  ======== */}
+{/* --------  -------- */}
+{/* ........  ........ */}
 
-export default function App() {
-  return (
-    <div className="App">
+
+{/* Doc Header */}
+ import React from "react";
+ import "./styles.css";
+
+ export default function App() {
+   return (
+     <div className="App">
      
-{/* Navbar */}
+{/* ======== Navbar ======================== */}
+  {/* -------- Navbar Row Container -------- */}
 
-      <div>
+      <div class="row, navbar, stickyTop">
 
+  {/* -------- Navbar Left Side ------------ */}
+        <div class="third, left">
+
+
+        </div>
+
+  {/* -------- Navbar Right Side ----------- */}
+        <div class="third, right">
+
+
+        </div>
+
+  {/* -------- Navbar Center --------------- */}
+        <div class="third, hMiddle">
+
+
+        </div>
       </div>
+  {/* ........................... Navbar End */}
 
-{/* Main Container */}
+{/* ======== Main Container ================ */}
+  {/* -------- Main Cont. Row Container ---- */}
+
+      <div class="row, mainContainer">
+
+  {/* -------- Splash "Page" --------------- */}
+        <div class="full, splash">
 
 
+        </div>
 
-{/* Footer */}
-      
+  {/* -------- Main Navigation ------------- */}
+        <div class = "row">
 
-    {/* End App */}</div>
+
+        </div>
+        
+      </div>
+  {/* ................... Main Container End */}
+
+{/* ======== Footer ======================== */}
+  {/* -------- Footer Row Container -------- */}
+
+      <div class="row">
+
+  {/* -------- Footer Top ------------------ */}
+        <div>
+
+          {/* -------- Footer Left --------- */}
+                <div>
+
+
+                </div>
+
+          {/* -------- Footer Right -------- */}
+                <div>
+
+
+                </div>
+
+        </div>{/* ........... Footer Top End */}
+
+  {/* -------- Footer Bottom --------------- */}
+        <div>
+
+
+        </div>
+      </div>
+  {/* ........................... Navbar End */}
+
+{/* ................................ End App */}
+    </div>
   );
 }
